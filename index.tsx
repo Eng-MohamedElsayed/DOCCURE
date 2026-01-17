1e3b18138e4cea3c7581bcaed475e7a59b5e0962
Mohamed Elsayed <131553546+Eng-MohamedElsayed@users.noreply.github.com>
2026-01-18 00:10:22 +0200

    Add Vercel Analytics to the application

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { Analytics } from "@vercel/analytics/react"
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
@@ -10,7 +10,8 @@ if (!rootElement) {
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
+  <React.StrictMode
+    <Analytics />
     <App />
   </React.StrictMode>
 );
